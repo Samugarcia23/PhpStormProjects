8e652eb03b646e9ce76a348a7f53e00d0169b587
Sam <samugarcia23@gmail.com>
2018-12-19 09:31:39 +0100

    seguimos con el login

--- EjemploLogin/php/login.php
+++ EjemploLogin/php/login.php
@@ -5,3 +5,16 @@
  * Date: 17/12/18
  * Time: 9:52
  */
+require_once "../bbdd/DatabaseModel.php";
+require_once "../clases/ConsUsuarioModel.php";
+
+$database = DatabaseModel::getInstance();
+$conexion = $database->getConnection();
+
+if($conexion->connect_error)
+{
+    trigger_error("Error al conectar a MySQL".$conexion->connect_error, E_USER_ERROR);
+}
+else{
+
+}
